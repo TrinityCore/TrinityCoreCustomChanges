54968ed969f9b1ae2d0d3c6b27b7d3c700c3698f
acidmanifesto <joshua.lee.betts@gmail.com>
2021-06-17 19:30:25 -0400

    Cleaner Drop then create table for solocraft.
    Originally i did not include a drop table. This should help prevent any conflict.

--- sql/custom/characters/characters.solocraft.sql
+++ sql/custom/characters/characters.solocraft.sql
@@ -1,9 +1,24 @@
--- Structure for table custom_solocraft_character_stats
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+-- Dumping structure for table characters.custom_solocraft_character_stats
+DROP TABLE IF EXISTS `custom_solocraft_character_stats`;
 CREATE TABLE IF NOT EXISTS `custom_solocraft_character_stats` (
-  `GUID` tinyint unsigned NOT NULL,
+  `GUID` tinyint(3) unsigned NOT NULL,
   `Difficulty` float NOT NULL,
-  `GroupSize` int NOT NULL,
-  `SpellPower` int unsigned NOT NULL DEFAULT '0',
+  `GroupSize` int(11) NOT NULL,
+  `SpellPower` int(10) unsigned NOT NULL DEFAULT '0',
   `Stats` float NOT NULL DEFAULT '100',
   PRIMARY KEY (`GUID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- Data exporting was unselected.
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;

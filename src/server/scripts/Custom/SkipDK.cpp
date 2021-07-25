1cd0b718176b7b9eb4b71a5eeca4366164a3acf0
acidmanifesto <joshua.lee.betts@gmail.com>
2021-07-25 19:24:45 -0400

    Added Missing Break

--- src/server/scripts/Custom/SkipDK.cpp
+++ src/server/scripts/Custom/SkipDK.cpp
@@ -201,7 +201,8 @@ public:
                 case LOCALE_esES: localizedEntry = LOCALE_LICHKING_6; break;
                 case LOCALE_esMX: localizedEntry = LOCALE_LICHKING_7; break;
                 case LOCALE_ruRU: localizedEntry = LOCALE_LICHKING_8; break;
-                case LOCALE_enUS: default: localizedEntry = LOCALE_LICHKING_0;
+                case LOCALE_enUS: localizedEntry = LOCALE_LICHKING_0; break;
+				default: localizedEntry = LOCALE_LICHKING_0;
                 }
                 AddGossipItemFor(player, GOSSIP_ICON_INTERACT_1, localizedEntry, GOSSIP_SENDER_MAIN, 11);
             }

fd4f1c8d6b9555a72d326ad28412d3b35127ab2b
acidmanifesto <joshua.lee.betts@gmail.com>
2021-07-26 14:54:41 +0200

    Add Optional Skip Module (#49)
    * Add Optional Skip Module
    
    * Merge remote-tracking branch 'origin/SkipDK335' into 3.3.5-skip-dk
    
    Co-Authored-By: Jinnaix <37972361+Jinnaix@users.noreply.github.com>
    
    * Update worldserver.conf.dist
    
    Further clarification of conf.
    
    Co-Authored-By: Jinnaix <37972361+Jinnaix@users.noreply.github.com>
    
    * remove unneeded action
    
    Co-Authored-By: Jinnaix <37972361+Jinnaix@users.noreply.github.com>
    
    * added extra new line in sql
    
    added extra new line in sql
    
    Co-Authored-By: Jinnaix <37972361+Jinnaix@users.noreply.github.com>
    
    * Update world.DKS.sql
    
    sql fix
    
    Co-Authored-By: Jinnaix <37972361+Jinnaix@users.noreply.github.com>
    
    * Create .gitignore
    
    * Added new line
    
    * Added Missing Break
    
    * tab to space adjustment
    
    Co-authored-by: Jinnaix <37972361+Jinnaix@users.noreply.github.com>

---
+++ sql/custom/world/world.DKS.sql
@@ -0,0 +1,4 @@
+-- npc id lich king
+-- Original npcflag was 2
+-- no previous scriptname
+UPDATE `creature_template` SET `npcflag`='3', `ScriptName`='npc_tc_skip_lich' WHERE  `entry`=25462;
--- src/server/scripts/Custom/SkipDK.cpp
+++ src/server/scripts/Custom/SkipDK.cpp
@@ -2,6 +2,7 @@
    Original Module From Single Player Project Consolidated Skip Module
    Rewritten for TC 335 By Single Player Project Developer MDic
    Original Concept from conanhun513
+   Assistance and Review by JinnaiX
 */
 
 #include "AccountMgr.h"
@@ -13,6 +14,9 @@
 #include "ObjectAccessor.h"
 #include "ObjectMgr.h"
 #include "Player.h"
+#include "ScriptMgr.h"
+#include "ScriptedCreature.h"
+#include "ScriptedGossip.h"
 #include "SharedDefines.h"
 #include "World.h"
 #include "WorldSession.h"
@@ -158,8 +162,146 @@ public:
     }
 };
 
+#define LOCALE_LICHKING_0 "I wish to skip the Death Knight starter questline."
+#define LOCALE_LICHKING_1 "죽음의 기사 스타터 퀘스트 라인을 건너뛰고 싶습니다."
+#define LOCALE_LICHKING_2 "Je souhaite sauter la série de quêtes de démarrage du Chevalier de la mort."
+#define LOCALE_LICHKING_3 "Ich möchte die Todesritter-Starter-Questreihe überspringen."
+#define LOCALE_LICHKING_4 "我想跳過死亡騎士新手任務線。"
+#define LOCALE_LICHKING_5 "我想跳過死亡騎士新手任務線。"
+#define LOCALE_LICHKING_6 "Deseo saltarme la línea de misiones de inicio del Caballero de la Muerte."
+#define LOCALE_LICHKING_7 "Deseo saltarme la línea de misiones de inicio del Caballero de la Muerte."
+#define LOCALE_LICHKING_8 "Я хочу пропустить начальную цепочку заданий Рыцаря Смерти."
+
+class Trinitycore_optional_deathknight_skip : public CreatureScript
+{
+public:
+    Trinitycore_optional_deathknight_skip() : CreatureScript("npc_tc_skip_lich") { }
+
+    struct npc_SkipLichAI : public ScriptedAI
+    {
+        npc_SkipLichAI(Creature* creature) : ScriptedAI(creature) { }
+
+        bool OnGossipHello(Player* player) override
+        {
+            if (me->IsQuestGiver())
+            {
+                player->PrepareQuestMenu(me->GetGUID());
+            }
+
+            if (sConfigMgr->GetBoolDefault("Skip.Deathknight.Optional.Enable", true))
+            {
+                char const* localizedEntry;
+                switch (player->GetSession()->GetSessionDbcLocale())
+                {
+                case LOCALE_koKR: localizedEntry = LOCALE_LICHKING_1; break;
+                case LOCALE_frFR: localizedEntry = LOCALE_LICHKING_2; break;
+                case LOCALE_deDE: localizedEntry = LOCALE_LICHKING_3; break;
+                case LOCALE_zhCN: localizedEntry = LOCALE_LICHKING_4; break;
+                case LOCALE_zhTW: localizedEntry = LOCALE_LICHKING_5; break;
+                case LOCALE_esES: localizedEntry = LOCALE_LICHKING_6; break;
+                case LOCALE_esMX: localizedEntry = LOCALE_LICHKING_7; break;
+                case LOCALE_ruRU: localizedEntry = LOCALE_LICHKING_8; break;
+                case LOCALE_enUS: localizedEntry = LOCALE_LICHKING_0; break;
+                default: localizedEntry = LOCALE_LICHKING_0;
+                }
+                AddGossipItemFor(player, GOSSIP_ICON_INTERACT_1, localizedEntry, GOSSIP_SENDER_MAIN, 11);
+            }
+
+            player->TalkedToCreature(me->GetEntry(), me->GetGUID());
+            SendGossipMenuFor(player, player->GetGossipTextId(me), me->GetGUID());
+            return true;
+        }
+
+        bool OnGossipSelect(Player* player, uint32 /*menuId*/, uint32 gossipListId) override
+        {
+            int DKL = sConfigMgr->GetFloatDefault("Skip.Deathknight.Start.Level", 58);
+            CloseGossipMenuFor(player);
+
+            switch (gossipListId)
+            {
+            case 11:
+                AddGossipItemFor(player, GOSSIP_ICON_INTERACT_1, "Yes", GOSSIP_SENDER_MAIN, 12);
+                AddGossipItemFor(player, GOSSIP_ICON_INTERACT_1, "No", GOSSIP_SENDER_MAIN, 13);
+                SendGossipMenuFor(player, player->GetGossipTextId(me), me->GetGUID());
+                break;
+
+            case 12:
+                if (player->GetLevel() <= DKL)
+                {
+                    player->SetLevel(DKL);
+                }
+                player->LearnSpell(53428, false);//runeforging
+                player->LearnSpell(53441, false);//runeforging
+                player->LearnSpell(53344, false);//runeforging
+                player->LearnSpell(62158, false);//runeforging
+                player->LearnSpell(33391, false);//journeyman riding
+                player->LearnSpell(54586, false);//runeforging credit
+                player->LearnSpell(48778, false);//acherus deathcharger
+                player->LearnSkillRewardedSpells(776, 375);//Runeforging
+                player->LearnSkillRewardedSpells(960, 375);//Runeforging
+                player->EquipNewItem(EQUIPMENT_SLOT_HEAD, 38661, true);//Greathelm of the Scourge Champion
+                player->EquipNewItem(EQUIPMENT_SLOT_WRISTS, 38666, true);//Plated Saronite Bracers
+                player->EquipNewItem(EQUIPMENT_SLOT_WAIST, 38668, true);//The Plaguebringer's Girdle
+                player->EquipNewItem(EQUIPMENT_SLOT_HANDS, 38667, true);//Bloodbane's Gauntlets of Command
+                player->EquipNewItem(EQUIPMENT_SLOT_CHEST, 38665, true);//Saronite War Plate
+                player->EquipNewItem(EQUIPMENT_SLOT_LEGS, 38669, true);//Engraved Saronite Legplates
+                player->EquipNewItem(EQUIPMENT_SLOT_SHOULDERS, 38663, true);// Blood-Soaked Saronite Plated Spaulders
+                player->EquipNewItem(EQUIPMENT_SLOT_FEET, 38670, true);//Greaves of the Slaughter
+                player->EquipNewItem(EQUIPMENT_SLOT_TRINKET1, 38675, true);//Signet of the Dark Brotherhood
+                player->EquipNewItem(EQUIPMENT_SLOT_TRINKET2, 38674, true);//Soul Harvester's Charm
+                player->EquipNewItem(EQUIPMENT_SLOT_FINGER1, 38671, true);//Valanar's Signet Ring
+                player->EquipNewItem(EQUIPMENT_SLOT_FINGER2, 38672, true);// Keleseth's Signet Ring
+                player->AddItem(39320, true);//Sky Darkener's Shroud of Blood
+                player->AddItem(38664, true);//Sky Darkener's Shroud of the Unholy
+                player->AddItem(39322, true);//Shroud of the North Wind
+                player->AddItem(38632, true);//Greatsword of the Ebon Blade
+                player->AddItem(6948, true);//Hearthstone
+                player->AddItem(38707, true);//Runed Soulblade
+                player->AddItem(40483, true);//Insignia of the Scourge
+
+                if (player->GetQuestStatus(12657) == QUEST_STATUS_NONE)//The Might Of The Scourge
+                {
+                    player->AddQuest(sObjectMgr->GetQuestTemplate(12657), nullptr);
+                    player->RewardQuest(sObjectMgr->GetQuestTemplate(12657), false, player);
+                }
+                if (player->GetQuestStatus(12801) == QUEST_STATUS_NONE)//The Light of Dawn
+                {
+                    player->AddQuest(sObjectMgr->GetQuestTemplate(12801), nullptr);
+                    player->RewardQuest(sObjectMgr->GetQuestTemplate(12801), false, player);
+                }
+                if (player->GetTeam() == ALLIANCE && player->GetQuestStatus(13188) == QUEST_STATUS_NONE)//Where Kings Walk
+                    player->AddQuest(sObjectMgr->GetQuestTemplate(13188), nullptr);
+                else if (player->GetTeam() == HORDE && player->GetQuestStatus(13189) == QUEST_STATUS_NONE)//Saurfang's Blessing
+                    player->AddQuest(sObjectMgr->GetQuestTemplate(13189), nullptr);
+                if (player->GetTeam() == ALLIANCE)
+                    player->TeleportTo(0, -8833.37f, 628.62f, 94.00f, 1.06f);//Stormwind
+                else
+                    player->TeleportTo(1, 1569.59f, -4397.63f, 16.06f, 0.54f);//Orgrimmar
+                ObjectAccessor::SaveAllPlayers();//Save
+                CloseGossipMenuFor(player);
+                break;
+
+            case 13://close
+                CloseGossipMenuFor(player);
+                break;
+
+            default:
+
+                break;
+            }
+            return true;
+        }
+    };
+
+    CreatureAI* GetAI(Creature* creature) const override
+    {
+        return new npc_SkipLichAI(creature);
+    }
+};
+
 void AddSC_skip_StarterArea()
 {
     new Trinitycore_skip_deathknight_announce;
     new Trinitycore_skip_deathknight;
+    new Trinitycore_optional_deathknight_skip;
 }

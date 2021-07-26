b67a99f23701921c9856fd1843e94c1bebe09c51
acidmanifesto <joshua.lee.betts@gmail.com>
2021-07-25 20:20:12 -0400

    tab to space

--- src/server/scripts/Custom/npc_arena1v1.h
+++ src/server/scripts/Custom/npc_arena1v1.h
@@ -48,67 +48,67 @@ bool config_BlockForbiddenTalents;
 
 void TC1V1CONFIG()
 {
-	config_Enable = sConfigMgr->GetBoolDefault("Arena.1v1.Enable", true);
-	config_minLevel = sConfigMgr->GetIntDefault("Arena.1v1.MinLevel", 255);
-	config_Costs = sConfigMgr->GetIntDefault("Arena.1v1.Costs", 400000);
-	config_BlockForbiddenTalents = sConfigMgr->GetBoolDefault("Arena.1v1.BlockForbiddenTalents", true);
+    config_Enable = sConfigMgr->GetBoolDefault("Arena.1v1.Enable", true);
+    config_minLevel = sConfigMgr->GetIntDefault("Arena.1v1.MinLevel", 255);
+    config_Costs = sConfigMgr->GetIntDefault("Arena.1v1.Costs", 400000);
+    config_BlockForbiddenTalents = sConfigMgr->GetBoolDefault("Arena.1v1.BlockForbiddenTalents", true);
 }
 
 // TalentTab.dbc -> TalentTabID
 const uint32 FORBIDDEN_TALENTS_IN_1V1_ARENA[] =
 {
-	// Healer
-	201, // PriestDiscipline
-	202, // PriestHoly
-	382, // PaladinHoly
-	262, // ShamanRestoration
-	282, // DruidRestoration
-
-	// Tanks
-	//383, // PaladinProtection
-	//163, // WarriorProtection
-
-	0 // End
+    // Healer
+    201, // PriestDiscipline
+    202, // PriestHoly
+    382, // PaladinHoly
+    262, // ShamanRestoration
+    282, // DruidRestoration
+    
+    // Tanks
+    //383, // PaladinProtection
+    //163, // WarriorProtection
+    
+    0 // End
 };
 
 // Return false, if player have invested more than 35 talentpoints in a forbidden talenttree.
 static bool Arena1v1CheckTalents(Player* player)
 {
-	if (!player)
-		return false;
+    if (!player)
+        return false;
 
-	if (config_BlockForbiddenTalents == false)
-		return true;
+    if (config_BlockForbiddenTalents == false)
+        return true;
 
-	uint32 count = 0;
-	for (uint32 talentId = 0; talentId < sTalentStore.GetNumRows(); ++talentId)
-	{
-		TalentEntry const* talentInfo = sTalentStore.LookupEntry(talentId);
+    uint32 count = 0;
+    for (uint32 talentId = 0; talentId < sTalentStore.GetNumRows(); ++talentId)
+    {
+        TalentEntry const* talentInfo = sTalentStore.LookupEntry(talentId);
 
-		if (!talentInfo)
-			continue;
+        if (!talentInfo)
+            continue;
 
-		for (int8 rank = MAX_TALENT_RANK - 1; rank >= 0; --rank)
-		{
-			if (talentInfo->SpellRank[rank] == 0)
-				continue;
+        for (int8 rank = MAX_TALENT_RANK - 1; rank >= 0; --rank)
+        {
+            if (talentInfo->SpellRank[rank] == 0)
+                continue;
 
-			if (player->HasTalent(talentInfo->SpellRank[rank], player->GetActiveSpec()))
-			{
-				for (int8 i = 0; FORBIDDEN_TALENTS_IN_1V1_ARENA[i] != 0; i++)
-					if (FORBIDDEN_TALENTS_IN_1V1_ARENA[i] == talentInfo->PrereqTalent)
-						count += rank + 1;
-			}
-		}
-	}
+            if (player->HasTalent(talentInfo->SpellRank[rank], player->GetActiveSpec()))
+            {
+                for (int8 i = 0; FORBIDDEN_TALENTS_IN_1V1_ARENA[i] != 0; i++)
+                    if (FORBIDDEN_TALENTS_IN_1V1_ARENA[i] == talentInfo->PrereqTalent)
+                        count += rank + 1;
+            }
+        }
+    }
 
-	if (count >= 36)
-	{
-		ChatHandler(player->GetSession()).SendSysMessage("You can't join, because you have invested too many points in a forbidden talent. Please edit your talents.");
-		return false;
-	}
-	else
-		return true;
+    if (count >= 36)
+    {
+        ChatHandler(player->GetSession()).SendSysMessage("You can't join, because you have invested too many points in a forbidden talent. Please edit your talents.");
+        return false;
+    }
+    else
+        return true;
 }
 
 #endif

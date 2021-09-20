907614038df625d87a38179e8e91105311aeb74a
acidmanifesto <joshua.lee.betts@gmail.com>
2021-09-20 11:59:50 -0400

    Mass Loot and Auto Mail Excess item
    Mass loot of items within 30 range.
    Mail Excess items to player from Mass looting.

--- src/server/game/Handlers/LootHandler.cpp
+++ src/server/game/Handlers/LootHandler.cpp
@@ -80,15 +80,56 @@ void WorldSession::HandleAutostoreLootItemOpcode(WorldPacket& recvData)
     else
     {
         Creature* creature = GetPlayer()->GetMap()->GetCreature(lguid);
-
-        bool lootAllowed = creature && creature->IsAlive() == (player->GetClass() == CLASS_ROGUE && creature->loot.loot_type == LOOT_PICKPOCKETING);
-        if (!lootAllowed || !creature->IsWithinDistInMap(_player, INTERACTION_DISTANCE))
+        if (!player->GetGroup())
         {
-            player->SendLootError(lguid, lootAllowed ? LOOT_ERROR_TOO_FAR : LOOT_ERROR_DIDNT_KILL);
-            return;
-        }
+            uint32 noSpaceForCount = 0;
 
-        loot = &creature->loot;
+            int i = 0;
+            float range = 30.0f;
+            Creature* c = nullptr;
+            std::vector<Creature*> creaturedie;
+            player->GetCreatureListWithEntryInGrid(creaturedie, creature->GetEntry(), range);
+            for (std::vector<Creature*>::iterator itr = creaturedie.begin(); itr != creaturedie.end(); ++itr)
+            {
+                c = *itr;
+                loot = &c->loot;
+
+                uint8 maxSlot = loot->GetMaxSlotInLootFor(player);
+                for (i = 0; i < maxSlot; ++i)
+                {
+                    LootItem* item = loot->LootItemInSlot(i, player);
+                    if (player->AddItem(item->itemid, item->count))
+                    {
+                        player->SendNotifyLootItemRemoved(lootSlot);
+                        player->SendLootRelease(player->GetLootGUID());
+                    }
+                    else
+                    {
+                        player->SendItemRetrievalMail(item->itemid, item->count);
+                        player->GetSession()->SendAreaTriggerMessage("Your items has been mailed to you.");
+                    }
+                }
+
+                loot->clear();
+
+                if (loot->isLooted() && loot->empty())
+                {
+                    c->RemoveFlag(UNIT_DYNAMIC_FLAGS, UNIT_DYNFLAG_LOOTABLE);
+                    c->AllLootRemovedFromCorpse();
+                }
+            }
+        }
+        else
+        {
+            bool lootAllowed = creature && creature->IsAlive() == (player->GetClass() == CLASS_ROGUE && creature->loot.loot_type == LOOT_PICKPOCKETING);
+            if (!lootAllowed || !creature->IsWithinDistInMap(_player, INTERACTION_DISTANCE))
+            {
+                player->SendLootError(lguid, lootAllowed ? LOOT_ERROR_TOO_FAR : LOOT_ERROR_DIDNT_KILL);
+                return;
+            }
+
+            loot = &creature->loot;
+        }
     }
 
     player->StoreLootItem(lootSlot, loot);
@@ -195,6 +236,24 @@ void WorldSession::HandleLootMoneyOpcode(WorldPacket& /*recvData*/)
         }
         else
         {
+            if (!player->GetGroup())
+            {
+                float range = 30.0f;
+                uint32 gold = 0;
+                Creature* creature = GetPlayer()->GetMap()->GetCreature(guid);
+                Creature* c = nullptr;
+                std::vector<Creature*> creaturedie;
+                player->GetCreatureListWithEntryInGrid(creaturedie, creature->GetEntry(), range);
+                for (std::vector<Creature*>::iterator itr = creaturedie.begin(); itr != creaturedie.end(); ++itr)
+                {
+                    c = *itr;
+                    loot = &c->loot;
+                    gold += loot->gold;
+                    loot->gold = 0;
+                }
+                loot->gold = gold;
+            }
+
             player->ModifyMoney(loot->gold);
             player->UpdateAchievementCriteria(ACHIEVEMENT_CRITERIA_TYPE_LOOT_MONEY, loot->gold);
 

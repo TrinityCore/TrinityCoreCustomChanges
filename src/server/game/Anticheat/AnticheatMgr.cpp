284331a9e29cd97b469f8ea812b2adafc1be9964
MDIC <joshua.lee.betts@gmail.com>
2022-03-26 10:33:10 -0400

    change logger

--- src/server/game/Anticheat/AnticheatMgr.cpp
+++ src/server/game/Anticheat/AnticheatMgr.cpp
@@ -623,7 +623,7 @@ void AnticheatMgr::BuildReport(Player* player, uint8 reportType)
     {
         if (sConfigMgr->GetBoolDefault("Anticheat.WriteLog", true))
         {
-            TC_LOG_INFO("module", "AnticheatMgr:: Reports reached assigned threshhold and counteracted by jailing player %s", player->GetGUID().ToString().c_str());
+            TC_LOG_INFO("anticheat", "AnticheatMgr:: Reports reached assigned threshhold and counteracted by jailing player %s", player->GetGUID().ToString().c_str());
         }
         // display warning at the center of the screen, hacky way?
         std::string str = "";

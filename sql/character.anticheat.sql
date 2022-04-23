0c4997a158b66229fc8b5821d4bcae5516aac96b
MDIC <joshua.lee.betts@gmail.com>
2022-04-23 07:36:06 -0400

    to sql standard adjusted

--- sql/character.anticheat.sql
+++ sql/character.anticheat.sql
@@ -13,10 +13,10 @@ CREATE TABLE IF NOT EXISTS `daily_players_reports` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 ALTER TABLE `daily_players_reports`
-	CHANGE COLUMN `climb_reports` `climb_reports` BIGINT UNSIGNED NOT NULL DEFAULT '0' AFTER `teleportplane_reports`,
-	ADD COLUMN `teleport_reports` BIGINT UNSIGNED NOT NULL DEFAULT '0' AFTER `climb_reports`,
-	ADD COLUMN `ignorecontrol_reports` BIGINT UNSIGNED NOT NULL DEFAULT '0' AFTER `teleport_reports`,
-	ADD COLUMN `zaxis_reports` BIGINT UNSIGNED NOT NULL DEFAULT '0' AFTER `ignorecontrol_reports`;
+	CHANGE COLUMN `climb_reports` `climb_reports` BIGINT UNSIGNED NOT NULL DEFAULT 0 AFTER `teleportplane_reports`,
+	ADD COLUMN `teleport_reports` BIGINT UNSIGNED NOT NULL DEFAULT 0 AFTER `climb_reports`,
+	ADD COLUMN `ignorecontrol_reports` BIGINT UNSIGNED NOT NULL DEFAULT 0 AFTER `teleport_reports`,
+	ADD COLUMN `zaxis_reports` BIGINT UNSIGNED NOT NULL DEFAULT 0 AFTER `ignorecontrol_reports`;
 
   
 CREATE TABLE IF NOT EXISTS `players_reports_status` (
@@ -34,7 +34,7 @@ CREATE TABLE IF NOT EXISTS `players_reports_status` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 ALTER TABLE `players_reports_status`
-	CHANGE COLUMN `climb_reports` `climb_reports` BIGINT UNSIGNED NOT NULL DEFAULT '0' AFTER `teleportplane_reports`,
-	ADD COLUMN `teleport_reports` BIGINT UNSIGNED NOT NULL DEFAULT '0' AFTER `climb_reports`,
-	ADD COLUMN `ignorecontrol_reports` BIGINT UNSIGNED NOT NULL DEFAULT '0' AFTER `teleport_reports`,
-	ADD COLUMN `zaxis_reports` BIGINT UNSIGNED NOT NULL DEFAULT '0' AFTER `ignorecontrol_reports`;
+	CHANGE COLUMN `climb_reports` `climb_reports` BIGINT UNSIGNED NOT NULL DEFAULT 0 AFTER `teleportplane_reports`,
+	ADD COLUMN `teleport_reports` BIGINT UNSIGNED NOT NULL DEFAULT 0 AFTER `climb_reports`,
+	ADD COLUMN `ignorecontrol_reports` BIGINT UNSIGNED NOT NULL DEFAULT 0 AFTER `teleport_reports`,
+	ADD COLUMN `zaxis_reports` BIGINT UNSIGNED NOT NULL DEFAULT 0 AFTER `ignorecontrol_reports`;

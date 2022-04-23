e1b83953d8acb99aaeb8f146db8a6f938e11d0a0
MDIC <joshua.lee.betts@gmail.com>
2022-04-23 07:30:03 -0400

    Revised Character SQL
    @Jinnaix had to re-explain to me what was ment in the review. my apologize @Aokromes

--- sql/character.anticheat.sql
+++ sql/character.anticheat.sql
@@ -1,4 +1,3 @@
-DROP TABLE IF EXISTS `daily_players_reports`;
 CREATE TABLE IF NOT EXISTS `daily_players_reports` (
   `guid` int unsigned NOT NULL DEFAULT 0,
   `creation_time` int unsigned NOT NULL DEFAULT 0,
@@ -10,13 +9,16 @@ CREATE TABLE IF NOT EXISTS `daily_players_reports` (
   `waterwalk_reports` bigint unsigned NOT NULL DEFAULT 0,
   `teleportplane_reports` bigint unsigned NOT NULL DEFAULT 0,
   `climb_reports` bigint unsigned NOT NULL DEFAULT 0,
-  `teleport_reports` bigint unsigned NOT NULL DEFAULT 0,
-  `ignorecontrol_reports` bigint unsigned NOT NULL DEFAULT 0,
-  `zaxis_reports` bigint unsigned NOT NULL DEFAULT 0,
   PRIMARY KEY (`guid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-DROP TABLE IF EXISTS `players_reports_status`;
+ALTER TABLE `daily_players_reports`
+	CHANGE COLUMN `climb_reports` `climb_reports` BIGINT UNSIGNED NOT NULL DEFAULT '0' AFTER `teleportplane_reports`,
+	ADD COLUMN `teleport_reports` BIGINT UNSIGNED NOT NULL DEFAULT '0' AFTER `climb_reports`,
+	ADD COLUMN `ignorecontrol_reports` BIGINT UNSIGNED NOT NULL DEFAULT '0' AFTER `teleport_reports`,
+	ADD COLUMN `zaxis_reports` BIGINT UNSIGNED NOT NULL DEFAULT '0' AFTER `ignorecontrol_reports`;
+
+  
 CREATE TABLE IF NOT EXISTS `players_reports_status` (
   `guid` int unsigned NOT NULL DEFAULT 0,
   `creation_time` int unsigned NOT NULL DEFAULT 0,
@@ -28,8 +30,11 @@ CREATE TABLE IF NOT EXISTS `players_reports_status` (
   `waterwalk_reports` bigint unsigned NOT NULL DEFAULT 0,
   `teleportplane_reports` bigint unsigned NOT NULL DEFAULT 0,
   `climb_reports` bigint unsigned NOT NULL DEFAULT 0,
-  `teleport_reports` bigint unsigned NOT NULL DEFAULT 0,
-  `ignorecontrol_reports` bigint unsigned NOT NULL DEFAULT 0,
-  `zaxis_reports` bigint unsigned NOT NULL DEFAULT 0,
   PRIMARY KEY (`guid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+ALTER TABLE `players_reports_status`
+	CHANGE COLUMN `climb_reports` `climb_reports` BIGINT UNSIGNED NOT NULL DEFAULT '0' AFTER `teleportplane_reports`,
+	ADD COLUMN `teleport_reports` BIGINT UNSIGNED NOT NULL DEFAULT '0' AFTER `climb_reports`,
+	ADD COLUMN `ignorecontrol_reports` BIGINT UNSIGNED NOT NULL DEFAULT '0' AFTER `teleport_reports`,
+	ADD COLUMN `zaxis_reports` BIGINT UNSIGNED NOT NULL DEFAULT '0' AFTER `ignorecontrol_reports`;

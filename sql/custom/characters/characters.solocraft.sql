d239a291ae002da012523fbe879b224792ce0bb8
MDIC <joshua.lee.betts@gmail.com>
2022-06-16 08:12:29 -0400

    Update characters.solocraft.sql

--- sql/custom/characters/characters.solocraft.sql
+++ sql/custom/characters/characters.solocraft.sql
@@ -1,9 +1,9 @@
 DROP TABLE IF EXISTS `custom_solocraft_character_stats`;
 CREATE TABLE IF NOT EXISTS `custom_solocraft_character_stats` (
-  `GUID` tinyint(3) unsigned NOT NULL,
+  `GUID` int unsigned NOT NULL DEFAULT 0,
   `Difficulty` float NOT NULL,
-  `GroupSize` int(11) NOT NULL,
-  `SpellPower` int(10) unsigned NOT NULL DEFAULT '0',
+  `GroupSize` int unsigned NOT NULL DEFAULT 0,
+  `SpellPower` int unsigned NOT NULL DEFAULT 0,
   `Stats` float NOT NULL DEFAULT '100',
   PRIMARY KEY (`GUID`)
 ) ENGINE=InnoDB COLLATE='utf8mb4_general_ci';

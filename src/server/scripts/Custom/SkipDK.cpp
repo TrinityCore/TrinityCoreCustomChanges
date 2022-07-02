abbd4ff8b2560cadf0e24082bdbc7c7e8470f204
MDIC <joshua.lee.betts@gmail.com>
2022-07-02 11:40:34 -0400

    fix (module\skip dk): Fix clear gossip menu

--- src/server/scripts/Custom/SkipDK.cpp
+++ src/server/scripts/Custom/SkipDK.cpp
@@ -248,7 +248,7 @@ public:
         {
             int DKL = sConfigMgr->GetFloatDefault("Skip.Deathknight.Start.Level", 58);
             CloseGossipMenuFor(player);
-
+            ClearGossipMenuFor(player);
             switch (gossipListId)
             {
             case OPTIONSKIPDK:

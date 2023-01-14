0a52140de08c3a7d41cd7fce5090f8c17687373c
M'Dic <joshua.lee.betts@gmail.com>
2023-01-13 19:55:37 -0500

    null check

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -12882,7 +12882,10 @@ void Unit::SendTeleportPacket(Position const& pos, bool teleportingTransport /*=
             transportPos.Relocate(x, y, z, o);
         }
     }
-    ToPlayer()->SetCanTeleport(true);
+    if (GetTypeId() == TYPEID_PLAYER)
+    {
+        ToPlayer()->SetCanTeleport(true);
+    }
     WorldPacket moveUpdateTeleport(MSG_MOVE_TELEPORT, 38);
     moveUpdateTeleport << GetPackGUID();
     Unit* broadcastSource = this;

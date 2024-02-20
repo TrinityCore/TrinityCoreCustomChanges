56761bb08c7136696c47b8be90565e6bacbebaa8
M'Dic <joshua.lee.betts@gmail.com>
2024-02-20 09:10:39 -0500

    Update cs_anticheat.cpp

--- src/server/scripts/Commands/cs_anticheat.cpp
+++ src/server/scripts/Commands/cs_anticheat.cpp
@@ -119,8 +119,8 @@ public:
         }
 
         WorldLocation loc = WorldLocation(1, 16226.5f, 16403.6f, -64.5f, 3.2f);// GM Jail Location
-        pTarget->TeleportTo(loc);
         pTarget->SetHomebind(loc, 876);// GM Jail Homebind location
+        pTarget->TeleportTo(loc);
 
         return true;
     }
